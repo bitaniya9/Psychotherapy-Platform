d9362f881533599264209a7335964cba2d134326
Bitaniya <bitaniyadamtew@gmail.com>
2025-10-26 17:23:21 +0300

    refactor:change function to class and add dependency injection

--- src/infrastructure/scheduler/TokenCleanup.ts
+++ src/infrastructure/scheduler/TokenCleanup.ts
@@ -2,19 +2,25 @@ import { NodeCronScheduler } from "./NodeCronScheduler";
 import { PrismaUserRepository } from "../database/UserRepository";
 import { CleanExpiredOTPUseCase } from "../../application/use-cases/auth/CleanExpiredOTPUseCase";
 
-export function startOTPCleanup(){
-    const scheduler=new NodeCronScheduler;
-    const userRepository=new PrismaUserRepository();
-    const useCase=new CleanExpiredOTPUseCase(userRepository);
-
-    scheduler.schedule("*/10 * * * *", async ()=>{
-        console.log("Starting OTP cleanup..")
-        await useCase.execute();
-    });
+export class startOTPCleanup{
     
-    // scheduler.schedule("*/1 * * * *", async ()=>{
+    constructor(
+        private scheduler:NodeCronScheduler,
+        // private userRepository:PrismaUserRepository,
+        private useCase:CleanExpiredOTPUseCase
+    ){}
+
+    start(){
+        this.scheduler.schedule("*/10 * * * *", async ()=>{
+            console.log("Starting OTP cleanup..")
+            await this.useCase.execute();
+        });
+    }
+}
+
+
+    
+    // this.scheduler.schedule("*/1 * * * *", async ()=>{
     //     console.log("Starting OTP cleanup..")
     //     await useCase.execute();
     // });
-
-}
